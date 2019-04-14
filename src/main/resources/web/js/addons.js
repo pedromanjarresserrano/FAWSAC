6611842cf60d9fb56eacef9ee3301f5c31ce74ac
Pedro M <pedro3manjarrez@hotmail.com>
2019-04-13 21:46:12 -0500

    fixes

--- src/main/resources/web/js/addons.js
+++ src/main/resources/web/js/addons.js
@@ -14,36 +14,5 @@ function loadChart(id, data) {
     }
 
 }
-        function selectItem() {
-          var padre = $(".container-v");
-          padre.each(function() {
-            $(this).on("click", function() {
-              padre.each(function() {
-                var p = $(this).find(".preview-v")[0];
-                p.style.background = "white";
-                p.style.color = "black";
-                var l = $(this).find(".label-v")[0];
-                l.style.background = "white";
-                l.style.color = "black";
-              });
-              var p = $(this).find(".preview-v")[0];
-              p.style.background = "black";
-              p.style.color = "white";
-              var l = $(this).find(".label-v")[0];
-              l.style.background = "black";
-              l.style.color = "white";
-              $(document).add(padre.clone());
-            });
-          });
-        }
-      var uuid = "";
-      var checkExist = setInterval(function () {
-        var list = $(".container-v");
-        if (list.length) {
-          if (list.get(0).id != uuid) {
-            selectItem();
-            uuid = list.get(0).id;
-          }
-        }
-      }, 1000);
+
 
